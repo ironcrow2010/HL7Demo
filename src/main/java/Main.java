611e799b26986eb3a5d42e87200214afa2fbdc3a
ironcrow2010 <ironcrow@qq.com>
2019-05-17 17:44:37 +0800

    尝试新增方法getInputHL7Message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,4 +9,8 @@ public class Main {
         String inputHL7Message = "MSH|^~\\&|||||||ADT^A01|a07749c8-ec04-4530-9065-46470ba0b4da||2.5.1\rEVN||20190516155145\rPID||abcde\\S\\f|||第一行\\X000d\\第二行\rPV1|1|有波浪\\R\\波浪\r";
         hl7Service.parseHL7MessageFromText(inputHL7Message);
     }
+
+    private static String getInputHL7Message(){
+        return "MSH|^~\\&|||||||ADT^A01|a07749c8-ec04-4530-9065-46470ba0b4da||2.5.1\rEVN||20190516155145\rPID||abcde\\S\\f|||第一行\\X000d\\第二行\rPV1|1|有波浪\\R\\波浪\r";
+    }
 }
