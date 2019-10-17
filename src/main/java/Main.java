849d0c418ddb3fe2fcf6270a962c98b025dde782
ironcrow2010 <ironcrow@qq.com>
2019-10-17 15:03:37 +0800

    1111

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import com.winning.hl7.PatientService;
 
 public class Main {
     public static void main(String[] args) throws HL7Exception {
+        // Push test
         PatientService service = new PatientService();
         service.initACK();
     }
